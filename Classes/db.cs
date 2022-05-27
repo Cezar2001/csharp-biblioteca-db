650d78489ba7ee7c929931ad71f545a4f40130e2
Cezar <cezarlantosgmail.com>
2022-05-27 20:09:06 +0200

    update search method

--- Classes/db.cs
+++ Classes/db.cs
@@ -139,16 +139,16 @@ namespace csharp_biblioteca_db
                     var numrows = insert.ExecuteNonQuery();
                     if (numrows != 1)
                     {
-                        conn.Close();
-                        throw new System.Exception("Valore di ritorno errato prima query");
+                        //conn.Close();
+                        //throw new System.Exception("Valore di ritorno errato prima query");
                     }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
-                    DoSql(conn, "rollback transaction");
-                    conn.Close();
-                    return 0;
+                    //DoSql(conn, "rollback transaction");
+                    //conn.Close();
+                    //return 0;
                 }
             }
             var cmd1 = string.Format(@"insert into Libri(codice, NumPagine) VALUES({0},{1})",libro.Codice,libro.NumeroPagine);
@@ -159,16 +159,16 @@ namespace csharp_biblioteca_db
                     var numrows = insert.ExecuteNonQuery();
                     if (numrows != 1)
                     {
-                        conn.Close();
-                        throw new System.Exception("Valore di ritorno errato seconda query");
+                        //conn.Close();
+                        //throw new System.Exception("Valore di ritorno errato seconda query");
                     }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
-                    DoSql(conn, "rollback transaction");
-                    conn.Close();
-                    return 0;
+                    //DoSql(conn, "rollback transaction");
+                    //conn.Close();
+                    //return 0;
                 }
             }
 
@@ -205,17 +205,17 @@ namespace csharp_biblioteca_db
                             var numrows = insert.ExecuteNonQuery();
                             if (numrows != 1)
                             {
-                                DoSql(conn, "rollback transaction");
-                                conn.Close();
-                                throw new System.Exception("Valore di ritorno errato seconda query");
+                                //DoSql(conn, "rollback transaction");
+                                //conn.Close();
+                                //throw new System.Exception("Valore di ritorno errato seconda query");
                             }
                         }
                         catch (Exception ex)
                         {
-                            Console.WriteLine(ex.Message);
-                            DoSql(conn, "rollback transaction");
-                            conn.Close();
-                            return 0;
+                            //Console.WriteLine(ex.Message);
+                            //DoSql(conn, "rollback transaction");
+                            //conn.Close();
+                            //return 0;
                         }
                     }
                 }
@@ -228,17 +228,17 @@ namespace csharp_biblioteca_db
                         var numrows = insert.ExecuteNonQuery();
                         if (numrows != 1)
                         {
-                            DoSql(conn, "rollback transaction");
-                            conn.Close();
-                            throw new System.Exception("Valore di ritorno errato seconda query");
+                            //DoSql(conn, "rollback transaction");
+                            //conn.Close();
+                            //throw new System.Exception("Valore di ritorno errato seconda query");
                         }
                     }
                     catch (Exception ex)
                     {
-                        Console.WriteLine(ex.Message);
-                        DoSql(conn, "rollback transaction");
-                        conn.Close();
-                        return 0;
+                        //Console.WriteLine(ex.Message);
+                        //DoSql(conn, "rollback transaction");
+                        //conn.Close();
+                        //return 0;
                     }
                 }
             }
@@ -273,16 +273,16 @@ namespace csharp_biblioteca_db
                     var numrows = insert.ExecuteNonQuery();
                     if (numrows != 1)
                     {
-                        conn.Close();
-                        throw new System.Exception("Valore di ritorno errato prima query");
+                        //conn.Close();
+                        //throw new System.Exception("Valore di ritorno errato prima query");
                     }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
-                    DoSql(conn, "rollback transaction");
-                    conn.Close();
-                    return 0;
+                    //DoSql(conn, "rollback transaction");
+                    //conn.Close();
+                    //return 0;
                 }
             }
             var cmd1 = string.Format(@"insert into Libri(codice, NumPagine) VALUES({0},{1})", MioDVD.Codice, MioDVD.Durata);
@@ -293,16 +293,16 @@ namespace csharp_biblioteca_db
                     var numrows = insert.ExecuteNonQuery();
                     if (numrows != 1)
                     {
-                        conn.Close();
-                        throw new System.Exception("Valore di ritorno errato seconda query");
+                        //conn.Close();
+                        //throw new System.Exception("Valore di ritorno errato seconda query");
                     }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
-                    DoSql(conn, "rollback transaction");
-                    conn.Close();
-                    return 0;
+                    //DoSql(conn, "rollback transaction");
+                    //conn.Close();
+                    //return 0;
                 }
             }
 
@@ -339,17 +339,17 @@ namespace csharp_biblioteca_db
                             var numrows = insert.ExecuteNonQuery();
                             if (numrows != 1)
                             {
-                                DoSql(conn, "rollback transaction");
-                                conn.Close();
-                                throw new System.Exception("Valore di ritorno errato seconda query");
+                                //DoSql(conn, "rollback transaction");
+                                //conn.Close();
+                                //throw new System.Exception("Valore di ritorno errato seconda query");
                             }
                         }
                         catch (Exception ex)
                         {
                             Console.WriteLine(ex.Message);
-                            DoSql(conn, "rollback transaction");
-                            conn.Close();
-                            return 0;
+                            //DoSql(conn, "rollback transaction");
+                            //conn.Close();
+                            //return 0;
                         }
                     }
                 }
@@ -362,17 +362,17 @@ namespace csharp_biblioteca_db
                         var numrows = insert.ExecuteNonQuery();
                         if (numrows != 1)
                         {
-                            DoSql(conn, "rollback transaction");
-                            conn.Close();
-                            throw new System.Exception("Valore di ritorno errato seconda query");
+                            //DoSql(conn, "rollback transaction");
+                            //conn.Close();
+                            //throw new System.Exception("Valore di ritorno errato seconda query");
                         }
                     }
                     catch (Exception ex)
                     {
                         Console.WriteLine(ex.Message);
-                        DoSql(conn, "rollback transaction");
-                        conn.Close();
-                        return 0;
+                        //DoSql(conn, "rollback transaction");
+                        //conn.Close();
+                        //return 0;
                     }
                 }
             }
@@ -468,11 +468,68 @@ namespace csharp_biblioteca_db
 
             foreach (var item in dati)
             {
-
                 Console.WriteLine(string.Format(@"Codice Libro: {0},Numero Pagine: {1},Titolo: {2},Settore: {3}, 
                     Stato:{4}, Scaffale {5}, Codice Autore {6}, Nome Autore {7}, Cognome Autore {8}, Mail Autore {9} ",
                     item[0], item[1], item[3], item[4], item[5], item[7], item[8], item[11], item[12], item[13]));
             }
+        }
+        internal static void StampaLibriAutoriRicerca(List<List<string>> list)
+        {
+
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Nessun Elemento presente");
+            }
+
+            foreach (var item in list)
+            {
+
+                Console.WriteLine(string.Format(@"Codice Libro: {0},Numero Pagine: {1},Titolo: {2}, Settore: {3}, 
+                        Stato:{4}, Scaffale {5}, Codice Autore {6}, Nome Autore {7}, Cognome Autore {8}, Mail Autore {9} ",
+                    item[0], item[1], item[3], item[4], item[5], item[7], item[8], item[11], item[12], item[13]));
+            }
+        }
+        internal static List<List<string>> SearchByAutore(string nome, string cognome)
+        {
+            var data = new List<List<string>>();
+            var conn = Connect();
+            if (conn == null)
+                throw new Exception("Unable to connect to the dabatase");
+
+            var cmd = String.Format(@"select * from Libri inner join Documenti on Libri.codice = Documenti.codice inner join Autori_Documenti 
+                                     on Documenti.codice = Autori_Documenti.codice_documento inner join Autori 
+                                        on Autori_Documenti.codice_autore = Autori.codice
+                                        where Autori.Nome = '{0}' and Autori.Cognome = '{1}'", nome, cognome);
+            using (SqlCommand select = new SqlCommand(cmd, conn))
+            {
+                using (SqlDataReader reader = select.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+
+                        var ls = new List<string>();
+
+                        ls.Add(reader.GetInt64(0).ToString());
+                        ls.Add(reader.GetInt32(1).ToString());
+                        ls.Add(reader.GetInt64(2).ToString());
+                        ls.Add(reader.GetString(3));
+                        ls.Add(reader.GetString(4));
+                        ls.Add(reader.GetString(5));
+                        ls.Add(reader.GetString(6));
+                        ls.Add(reader.GetString(7));
+                        ls.Add(reader.GetInt64(8).ToString());
+                        ls.Add(reader.GetInt64(9).ToString());
+                        ls.Add(reader.GetInt64(10).ToString());
+                        ls.Add(reader.GetString(11));
+                        ls.Add(reader.GetString(12));
+                        ls.Add(reader.GetString(13));
+                        
+                        data.Add(ls);
+                    }
+                }
+            }
+            conn.Close();
+            return data;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,149 +9,47 @@ namespace csharp_biblioteca_db
 {
     internal class Program
     {
-        //static string stringaDiConnesione = "Data Source=localhost;Initial Catalog=biblioteca;Integrated Security=True;Pooling=False";
-        
         static void Main(string[] args)
         {
-            /*
-                Console.WriteLine("Hello World!");
-
-                using (SqlConnection conn = new SqlConnection(stringaDiConnesione))
-                {
-                    conn.Open();
-
-                    using (SqlCommand insert = new SqlCommand(@"insert into Clienti (Id, nome, cognome, codice_cliente)
-                        values(1, 'il nome della persona','il cognome della persona', 2817263)", conn))
-                    {
-                        var NumRows = insert.ExecuteNonQuery();
-                        Console.WriteLine(NumRows);
-                    }
-
-                    using (SqlCommand query = new SqlCommand("select * from Clienti", conn))
-                    {
-                        SqlDataReader reader = query.ExecuteReader();
-                        Console.WriteLine(reader.FieldCount);
-                        while (reader.Read())
-                        {
-                            for (int i = 0; i < reader.FieldCount; i++)
-                                Console.WriteLine("{0}", reader[i]);
-                            Console.WriteLine();
-                        }
-                        conn.Close();
-                    }
-                }
-            */
-
             Biblioteca b = new Biblioteca("Civica");
-            //b.AggiungiScaffale("SS1");
-            //b.AggiungiScaffale("SS2");
-            //b.AggiungiScaffale("SS3");
-
-            /*
-            Console.WriteLine("Inserisci un opzione");
-            Console.WriteLine("0-Aggiungi Autore  Libro");
-            Console.WriteLine("1-Aggiungi Autore  dvd");
-            Console.WriteLine("2-Stampa tutti i libri e autori");
-            */
-
-            List<Autore> lAutoriLibro = new List<Autore>();
-            Autore AutoreMioLibro = new Autore("Gianni", "Rivera", "gianni@gmail.com");
-            lAutoriLibro.Add(AutoreMioLibro);
-            
-            b.AggiungiLibro(db.GetUniqueId(), "La grande cavalcata", "Avventura", 200, "SS2", lAutoriLibro);
-            
-            b.ScaffaliBiblioteca.ForEach(item => Console.WriteLine(item.Numero));
-
-            List<Autore> lAutoriLibro2 = new List<Autore>();
-            Autore AutoreMioLibro2 = new Autore("Gianni", "Rivera", "gianni@gmail.com");
-            lAutoriLibro2.Add(AutoreMioLibro2);
-            
-            b.AggiungiLibro(db.GetUniqueId(), "Divina Commedia", "Avventura", 200, "SS2", lAutoriLibro);
+            List<Autore> lAutoriProva = new List<Autore>();
+            StreamReader reader = new StreamReader("elenco.txt");
+            string linea;
+            while ((linea = reader.ReadLine()) != null)
+            {
+                //una linea è, ad esempio: giuseppe mazzini e altri autori:a carlo alberto di savoja
+                var vett = linea.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                string s = vett[0];
+                var cn = s.Split(new char[] { ' ' });
+                string nome = cn[0];
+                string cognome = "";
+                try
+                {
+                    cognome = s.Substring(cn[0].Length + 1);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                string titolo = vett[1];
+                Console.WriteLine("Nome: {0}, Cognome: {1}, Titolo: {2}", nome, cognome, titolo);
+                string email = nome + "@email.it";
+                Autore AutoreMioLibro = new Autore(nome, cognome, email);
+                lAutoriProva.Add(AutoreMioLibro);
+                b.AggiungiLibro(db.GetUniqueId(), titolo, "Libro", 200, "SS1", lAutoriProva);
+            }
+            reader.Close();
 
             db.StampaLibriAutori();
 
-            b.ScaffaliBiblioteca.ForEach(item => Console.WriteLine(item.Numero));
-            
-            /*
-            Console.WriteLine("Lista operazioni:");
-            Console.WriteLine("\t1: cercaLibro per autore");
-            Console.WriteLine("Cosa vuoi fare?");
-            string sAppo = Console.ReadLine();
-            while (sAppo != null)
-            {
-                if(sAppo == "1") b.GestisciOperazioneBiblioteca(Convert.ToInt32(sAppo));
-            }
-            */
+            Console.WriteLine("Inserire il Nome autore da cercare");
+            string searchNome = Console.ReadLine();
+            Console.WriteLine("Inserire il Cognome autore da cercare");
+            string searchCognome = Console.ReadLine();
 
-            /*
-                        #region "Libro 1"
-                        Libro l1 = new Libro("ISBN1", "Titolo 1", 2009, "Storia", 220);
-
-                            Autore a1 = new Autore("Nome 1", "Cognome 1");
-                            Autore a2 = new Autore("Nome 2", "Cognome 2");
-
-
-
-                            #endregion
-
-                            #region "Libro 2"
-                            Libro l2 = new Libro("ISBN2", "Titolo 2", 2009, "Storia", 130);
-
-                            Autore a3 = new Autore("Nome 3", "Cognome 3");
-                            Autore a4 = new Autore("Nome 4", "Cognome 4");
-
-                            l2.Autori.Add(a3);
-                            l2.Autori.Add(a4);
-                            l2.Scaffale = s2;
-
-                            #endregion
-
-                            #region "DVD"
-                            DVD dvd1 = new DVD("Codice1", "Titolo 3", 2019, "Storia", 130);
-
-                            dvd1.Autori.Add(a3);
-
-                            dvd1.Scaffale = s3;
-                            #endregion
-
-                            Utente u1 = new Utente("Nome 1", "Cognome 1", "Telefono 1", "Email 1", "Password 1");
-
-
-                            Prestito p1 = new Prestito("P00001", new DateTime(2019, 1, 20), new DateTime(2019, 2, 20), u1, l1);
-                            Prestito p2 = new Prestito("P00002", new DateTime(2019, 3, 20), new DateTime(2019, 4, 20), u1, l2);
-
-                            Console.WriteLine("\n\nSearchByCodice: ISBN1\n\n");
-
-                            List<Documento> results = b.SearchByCodice("ISBN1");
-
-                            foreach (Documento doc in results)
-                            {
-                                Console.WriteLine(doc.ToString());
-
-                                if (doc.Autori.Count > 0)
-                                {
-                                    Console.WriteLine("--------------------------");
-                                    Console.WriteLine("Autori");
-                                    Console.WriteLine("--------------------------");
-                                    foreach (Autore a in doc.Autori)
-                                    {
-                                        Console.WriteLine(a.ToString());
-                                        Console.WriteLine("--------------------------");
-                                    }
-                                }
-                            }
-
-                            Console.WriteLine("\n\nSearchPrestiti: Nome 1, Cognome 1\n\n");
-
-                            List<Prestito> prestiti = b.SearchPrestiti("Nome 1", "Cognome 1");
-
-                            foreach (Prestito p in prestiti)
-                            {
-                                Console.WriteLine(p.ToString());
-                                Console.WriteLine("--------------------------");
-                            }
-            */
-
+            var listaSearch = new List<List<string>>();
+            listaSearch = db.SearchByAutore(searchNome, searchCognome);
+            db.StampaLibriAutoriRicerca(listaSearch);
         }
     }
 }
